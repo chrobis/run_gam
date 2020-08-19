ce8f7eaa92091fd0d5d87142a5ec25832bccd6b5
chrobis <chrobis@gmail.com>
2020-08-19 13:14:37 -0700

    changing workday fields

--- run_gam_tool_box/update_google.py
+++ run_gam_tool_box/update_google.py
@@ -7,17 +7,23 @@ Kick off the gam update
 def run(file_path_to_csv):
     command = f"../bin/gam/gam csv {file_path_to_csv} gam ~Action " \
               f"user '~Email Address' " \
-              f"firstname '~First Name' " \
-              f"lastname '~Last Name' " \
-              f"Employee_Information.City '~City' " \
-              f"Employee_Information.State '~Building_ID' " \
-              f"Employee_Information.Country_Code '~State' " \
-              f"Employee_Information.Country '~Country Code' " \
-              f"Workday_Custom_Attribute.Workday_Employee_ID '~Workday Employee ID' " \
-              f"Workday_Custom_Attribute.Workday_Manager_ID '~Workday Manager ID' " \
+              f"firstname '~Preferred First Name' " \
+              f"lastname '~Preferred Last Name' " \
+              f"Employee_Office_Location.Work_Location '~Building_ID' " \
+              f"Employee_Office_Location.Work_Address '~Work Address' " \
+              f"Employee_Office_Location.City '~City' " \
+              f"Employee_Office_Location.State '~State' " \
+              f"Employee_Office_Location.Country_Code '~Country Code' " \
+              f"Employee_Record_Info.Legal_Last_Name '~Legal Last Name' " \
+              f"Employee_Record_Info.Legal_First_Name '~Legal First Name' " \
+              f"Employee_Record_Info.Hire_Date '~Hire Date' " \
+              f"Employee_Record_Info.Employee_ID '~Workday Employee ID' " \
+              f"Employee_Record_Info.Manager_ID '~Workday Manager ID' " \
+              f"Employee_Record_Info.Is_a_Manager '~IsManager' " \
               f"relation manager '~Managers Email' " \
               f"organization title '~Employee Title' " \
-              f"department '~Cost Center' primary"
+              f"costCenter '~Cost Center ID' " \
+              f"department '~Cost Center Name' primary"
 
     return command
 
