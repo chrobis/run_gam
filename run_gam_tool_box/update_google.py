e607ac71c9323e7a02b92b330786e829fe22a858
chrobis <chrobis@gmail.com>
2020-08-20 13:20:21 -0700

    changing workday fields

--- run_gam_tool_box/update_google.py
+++ run_gam_tool_box/update_google.py
@@ -9,6 +9,7 @@ def run(file_path_to_csv):
               f"user '~Email Address' " \
               f"firstname '~Preferred First Name' " \
               f"lastname '~Preferred Last Name' " \
+              f"otheremail home '~Personal Email Address' " \
               f"Employee_Office_Location.Work_Location '~Building_ID' " \
               f"Employee_Office_Location.Work_Address '~Work Address' " \
               f"Employee_Office_Location.City '~City' " \
@@ -21,7 +22,9 @@ def run(file_path_to_csv):
               f"Employee_Record_Info.Manager_ID '~Workday Manager ID' " \
               f"Employee_Record_Info.Is_a_Manager '~IsManager' " \
               f"relation manager '~Managers Email' " \
+              f"location type default area '~Building_ID' endlocation " \
               f"organization title '~Employee Title' " \
+              f"description '~Employee Type' " \
               f"costCenter '~Cost Center ID' " \
               f"department '~Cost Center Name' primary"
 
